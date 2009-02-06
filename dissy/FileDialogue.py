47ee0f05525514c8f6263c833ad61f86944fa1ef
Simon Kagstrom <simon.kagstrom@gmail.com>
2009-02-06 18:00:49 +0000

    File dialogue/about dialogue fixes from Lubomir

--- dissy/FileDialogue.py
+++ dissy/FileDialogue.py
@@ -20,18 +20,27 @@ import dissy.FunctionModel
 
 class FileDialogue:
     def __init__(self, controller):
-	filesel = gtk.FileSelection(title=None)
-	# Connect the ok_button to file_ok_sel method
-	filesel.ok_button.connect("clicked", self.okSelected, filesel, controller)
+        file_open_dialog = gtk.FileChooserDialog(title="Open object file",
+            action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                buttons=(gtk.STOCK_CANCEL,
+                        gtk.RESPONSE_CANCEL,
+                        gtk.STOCK_OPEN,
+                        gtk.RESPONSE_OK))
+        filter = gtk.FileFilter()
+        filter.set_name("Object files")
+        filter.add_pattern("*.o")
+        filter.add_mime_type("application/x-object")
+        filter.add_mime_type("application/x-executable")
+        file_open_dialog.add_filter(filter)
+        filter = gtk.FileFilter()
+        filter.set_name("All files")
+        filter.add_pattern("*")
+        file_open_dialog.add_filter(filter)
 
-	# Connect the cancel_button to destroy the file selector widget
-	filesel.cancel_button.connect("clicked",
-				      lambda w: filesel.destroy())
-	filesel.show()
-
-    def okSelected(self, widget, filesel, controller):
-	name = filesel.get_filename()
-
-	filesel.destroy()
-
-	controller.loadFile(name)
+        if file_open_dialog.run() == gtk.RESPONSE_OK:
+            filename = file_open_dialog.get_filename()
+            controller.loadFile(filename)
+            file_open_dialog.destroy()
+        else:
+            file_open_dialog.destroy()
+            return
