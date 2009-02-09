60aeb8dfeb33e5cffe013a81e69a53a787856688
Simon Kagstrom <simon.kagstrom@gmail.com>
2009-02-09 17:57:29 +0000

    Some instr descriptions for MIPS

--- dissy/mips.py
+++ dissy/mips.py
@@ -34,6 +34,24 @@ mips_jumps = ['bgez',
 
 mips_calls = ['jal']
 
+instr_descriptions = {
+    'lbu': 'Load Unsigned Byte',
+    'lb': 'Load signed Byte',
+    'li': 'Load Immediate value to destination register',
+    'lui' : 'Load Upper Immediate (set destination to v << 16)',
+    'move' : 'Copy between registers',
+    'jal' : 'Jump And Link, jump to destination and store return address in ra (r31)',
+    'or' : 'Bitwise or',
+    'sb' : 'Store Byte to memory',
+    'sw' : 'Store 32-bit word to memory',
+    'sh' : 'Store 16-bit word to memory',
+    'lb' : 'Load Byte from memory, signed',
+    'lw' : 'Load 32-bit value from memory',
+    'lh' : 'Load 16-bit value from memory, signed',
+    'lbu' : 'Load 16-bit value from memory, unsigned',
+    'lhu' : 'Load 16-bit value from memory, unsigned',
+}
+
 
 class MipsArchitecture(Architecture):
     def __init__(self):
@@ -45,3 +63,12 @@ class MipsArchitecture(Architecture):
         if r == None:
             return Architecture.getJumpDestination(self, insn, args)
         return Architecture.getJumpDestination(self, insn, r.group(2))
+
+    def getInstructionInfo(self, instruction):
+        opcode = instruction.getOpcode()
+        args = str(instruction.getArgs())
+        description = instr_descriptions.get(instruction.getOpcode(), '')
+
+        return {'shortinfo': opcode + " " + args,
+                'description': description,
+                }
