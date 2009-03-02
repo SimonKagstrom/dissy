047c50d1c920fad6b26e9a962c6a198e443fc5ca
Mads Chr. Olesen <shiyee@gmail.com>
2009-03-02 08:23:42 +0000

    For ARM only:  * Add function to parse the arguments of an instruction, and report which    registers are read and written; This is needed if one wants to do a value    analysis, but is unused ATM.  * Testcases for above.

--- dissy/arm.py
+++ dissy/arm.py
@@ -55,39 +55,53 @@ Canonical form of "ldm SP!, <reglist>\"""",
     'muls': 'Multiply and set condition flags',
     'mla': 'Multiply and Accumulate',
     'mls': 'Multiply and Subtract',
+    'smull': """Signed Multiply Long
+%(arg1)s,%(arg2)s := signed(%(arg3)s * %(arg4)s)""",
+    'ldr': 'Load (from memory to register)',
+    'str': 'Store (from register to memory)',
+    'lsl': 'Logical Shift Left',
+    'lsls': 'Logical Shift Left and set condition flags',
+    'lsr': 'Logical Shift Right',
+    'lsr': 'Logical Shift Right and set condition flags',
+    'rsb': 'Reverse Subtract',
 }
+
+conditions = {
+    'cs': 'Carry Set',
+    'cc': 'Carry Clear',
+    'eq': 'Equal (Zero Set)',
+    'ne': 'Not Equal (Zero Clear)',
+    'vs': 'Overflow Set',
+    'vc': 'Overflow Clear',
+    'gt': 'Signed Greater Than',
+    'lt': 'Signed Less Than',
+    'ge': 'Signed Greater than or Equal',
+    'le': 'Signed Less than or Equal',
+    'pl': 'Plus (Positive)',
+    'mi': 'Minus (Negative)',
+    'hi': 'Unsigned Higher Than',
+    'lo': 'Unsigned Lower Than',
+    'hs': 'Unsigned Higher or Same',
+    'ls': 'Unsigned Lower or Same',
+    }
 
 arm_lists_inited = False
 if not arm_lists_inited:
-    branch_instructions = {
+    conditional_instructions = {
         'b': """Branch on %s
 PC := label, label is this instruction +/-32Mb""",
         'bx': """Branch and eXchange on %s
-PC := Rm""",
+PC := Rm
+Change to Thumb mode if Rm[0] is 1, change to ARM mode if Rm[0] is 0""",
         'bl': """Branch and Link on %s""",
         'mov': """Move on %s""",
+        'add': """Add on %s""",
+        'sub': """Subtract on %s""",
         }
-    conditions = {
-        'cs': 'Carry Set',
-        'cc': 'Carry Clear',
-        'eq': 'Equal (Zero Set)',
-        'ne': 'Not Equal (Zero Clear)',
-        'vs': 'Overflow Set',
-        'vc': 'Overflow Clear',
-        'gt': 'Signed Greater Than',
-        'lt': 'Signed Less Than',
-        'ge': 'Signed Greater than or Equal',
-        'le': 'Signed Less than or Equal',
-        'pl': 'Plus (Positive)',
-        'mi': 'Minus (Negative)',
-        'hi': 'Unsigned Higher Than',
-        'lo': 'Unsigned Lower Than',
-        'hs': 'Unsigned Higher or Same',
-        'ls': 'Unsigned Lower or Same',
-        }
-    for i in branch_instructions:
+
+    for i in conditional_instructions:
         for c in conditions:
-            arm_instr_descriptions[i + c] = branch_instructions[i] % (conditions[c])
+            arm_instr_descriptions[i + c] = conditional_instructions[i] % (conditions[c])
             arm_conditionflag_users += [i + c]
     arm_lists_inited = True
 
@@ -99,8 +113,115 @@ class ArmArchitecture(architecture.Architecture):
     def getInstructionInfo(self, instruction):
         opcode = instruction.getOpcode()
         args = str(instruction.getArgs())
+
+        args_list = args.split(", ")
+        args_dict = dict(
+            zip(['arg' + str(i) for i in range(1, len(args_list)+1)],
+                args_list))
         description = arm_instr_descriptions.get(instruction.getOpcode(), '')
 
         return {'shortinfo': opcode + " " + args,
-                'description': description,
+                'description': description % args_dict,
                 }
+
+    def parseArguments(self, instr):
+        """Parses an argument string, into a two-tuple, containing:
+            * A list of the registers read in this instruction
+            * A list of the registers written to in this instruction"""
+
+        def parseComSepList(lstr):
+            ret = []
+            tmp = ""
+            stack = []
+            for c in lstr:
+                if c in "{[":
+                    stack += [c]
+                    tmp += c
+                elif c in "]}":
+                    tmp += c
+                    c2 = stack.pop()
+                    if c == "]" and c2 == "[" or \
+                        c == "}" and c2 == "{":
+                        ret += [tmp]
+                        tmp = ""
+                    else:
+                        raise ValueError("unbalanced parenthesis")
+                elif c == ' ':
+                    pass
+                elif stack == [] and c == ',':
+                    ret += [tmp]
+                    tmp = ""
+                elif stack == []:
+                    tmp += c
+                else:
+                    tmp += c
+            if tmp:
+                ret += [tmp]
+            return ret
+        def isRegister(s):
+            if s in ['r' + str(i) for i in range(0, 16)]: #r0..r15
+                return True
+            if s in ['sl', #r10 = gcc "got pointer"?
+                    'fp', #r11 = gcc frame pointer
+                    'ip', #r12 = gcc "scratch register"?
+                    'sp', #r13, stack pointer
+                    'lr', #r14, link register
+                    'pc']: #r15, program counter
+                return True
+            return False
+
+        args = parseComSepList(instr.args)
+        if instr.getOpcode()[:3] == 'cmp':
+            regwrite = []
+            regread = [a for a in args if isRegister(a)]
+        elif instr.getOpcode()[:3] in ['sub', 'add', 'lsl', 'asr', 'rsb', 'mov']:
+            regwrite = [args[0]]
+            regread = [a for a in args[1:] if isRegister(a)]
+        #branches
+        elif instr.getOpcode() in ['b' + c for c in conditions.keys() + ['']]:
+            regwrite = ['pc']
+            regread = []
+        elif instr.getOpcode() in ['bl' + c for c in conditions.keys() + ['']]:
+            regwrite = ['pc', 'lr']
+            regread = []
+        elif instr.getOpcode() in ['bx' + c for c in conditions.keys() + ['']]:
+            regwrite = ['pc']
+            regread = [args[0]]
+        elif instr.getOpcode() in ['blx' + c for c in conditions.keys() + ['']]:
+            regwrite = ['pc']
+            regread = isRegister(args[0]) and [args[0]] or []
+        #load
+        elif instr.getOpcode() in ['ldr' + c for c in conditions.keys() + ['']]:
+            regwrite = [args[0]]
+            regread = []
+            if args[1].startswith('['):
+                offsetl = parseComSepList(args[1][1:-1])
+                regread = [r for r in offsetl if isRegister(r)]
+        #store
+        elif instr.getOpcode() in ['str' + c for c in conditions.keys() + ['']]:
+            regwrite = []
+            regread = [args[0]]
+            if args[1].startswith('['):
+                offsetl = parseComSepList(args[1][1:-1])
+                regread += [r for r in offsetl if isRegister(r)]
+        #push
+        elif instr.getOpcode() in ['push' + c for c in conditions.keys() + ['']]:
+            regwrite = ['sp']
+            regread = ['sp']
+            reglist = parseComSepList(args[0][1:-1])
+            regread += reglist
+        elif instr.getOpcode() in ['pop' + c for c in conditions.keys() + ['']]:
+            regwrite = ['sp']
+            regread = ['sp']
+            reglist = parseComSepList(args[0][1:-1])
+            regwrite += reglist
+        elif instr.getOpcode() in ['smull' + c for c in conditions.keys() + ['']]:
+            regwrite = [args[0], args[1]]
+            regread = [args[2], args[3]]
+        elif instr.getOpcode() in ['mul' + c for c in conditions.keys() + ['']]:
+            regwrite = [args[0]]
+            regread = [args[1], args[2]]
+        else:
+            raise ValueError("Unknown instruction opcode: " + str(instr))
+
+        return (regread, regwrite)
---
+++ tests/test_arm_arch.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+import sys
+import os.path
+import unittest
+projdir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+sys.path = [projdir] + sys.path
+import dissy.architecture
+from dissy.arm import ArmArchitecture
+from dissy.Instruction import Instruction
+
+
+def instr(opcode, args):
+    #mock of function + file object
+    class MockFunction():
+        def __init__(self):
+            self.baseAddress = 0
+            self.arch = ArmArchitecture()
+        def getFile(self):
+            return self
+        def getArch(self):
+            return self.arch
+    return Instruction(MockFunction(), 0, None, opcode, args)
+
+class TestArmArch(unittest.TestCase):
+    def setUp(self):
+        self.arch = ArmArchitecture()
+        self.assertNotEqual(self.arch, None)
+
+    def test_arg_parsing(self):
+        self.assertEqual(self.arch.parseArguments(instr('mov', 'r8, #6')),
+            ([], ['r8']))
+        self.assertEqual(self.arch.parseArguments(instr('mov', 'ip, #0')),
+            ([], ['ip']))
+        self.assertEqual(self.arch.parseArguments(instr('movle', 'r0, #1')),
+            ([], ['r0']))
+        self.assertEqual(self.arch.parseArguments(instr('cmp', 'r0, #1')),
+            (['r0'], []))
+
+        #Add og sub
+        self.assertEqual(self.arch.parseArguments(instr('sub', 'sp, sp, #4')),
+            (['sp'], ['sp']))
+        self.assertEqual(self.arch.parseArguments(instr('add', 'sp, sp, #4')),
+            (['sp'], ['sp']))
+        self.assertEqual(self.arch.parseArguments(instr('add', 'r2, r2, #1')),
+            (['r2'], ['r2']))
+        self.assertEqual(self.arch.parseArguments(instr('addlt', 'r5, r5, r3')),
+            (['r5', 'r3'], ['r5']))
+        self.assertEqual(self.arch.parseArguments(instr('sub', 'r3, r3, r0, lsl #2')),
+            (['r3', 'r0'], ['r3']))
+
+        #shifts
+        self.assertEqual(self.arch.parseArguments(instr('lsl', 'r1, r3, #7')),
+            (['r3'], ['r1']))
+        self.assertEqual(self.arch.parseArguments(instr('asr', 'r1, r2, #31')),
+            (['r2'], ['r1']))
+        self.assertEqual(self.arch.parseArguments(instr('rsb', 'r0, r1, r0, asr #9')),
+            (['r1', 'r0'], ['r0']))
+        self.assertEqual(self.arch.parseArguments(instr('lsl', 'r1, r3, #7')),
+            (['r3'], ['r1']))
+
+        #branches
+        self.assertEqual(self.arch.parseArguments(instr('bl', '0')),
+            ([], ['pc', 'lr']))
+        self.assertEqual(self.arch.parseArguments(instr('bx', 'lr')),
+            (['lr'], ['pc']))
+        self.assertEqual(self.arch.parseArguments(instr('ble', '4c')),
+            ([], ['pc']))
+
+        #Dynamisk load
+        self.assertEqual(self.arch.parseArguments(instr('ldr', 'r3, [ip]')),
+            (['ip'], ['r3']))
+        self.assertEqual(self.arch.parseArguments(instr('ldr', 'r3, [pc, #8]')),
+            (['pc'], ['r3']))
+        self.assertEqual(self.arch.parseArguments(instr('ldr', 'r3, [r2, r0]')),
+            (['r2', 'r0'], ['r3']))
+        #Dynamisk store
+        self.assertEqual(self.arch.parseArguments(instr('str', 'r0, [r3]')),
+            (['r0', 'r3'], []))
+        self.assertEqual(self.arch.parseArguments(instr('str', 'r0, [r4, r5]')),
+            (['r0', 'r4', 'r5'], []))
+
+        #unimplemented instruction type
+        self.assertRaises(ValueError, self.arch.parseArguments, instr('abemad', 'r1, r0'))
+
+        #multiplication
+        self.assertEqual(self.arch.parseArguments(instr('smull', 'r1, r0, r3, r2')),
+            (['r3', 'r2'], ['r1', 'r0']))
+
+        #multiplication
+        self.assertEqual(self.arch.parseArguments(instr('mul', 'r0, r3, r2')),
+            (['r3', 'r2'], ['r0']))
+
+        #Push, pop
+        self.assertEqual(self.arch.parseArguments(instr('push', '{lr}')),
+            (['sp', 'lr'], ['sp']))
+        self.assertEqual(self.arch.parseArguments(instr('push', '{r4, r5, lr}')),
+            (['sp', 'r4', 'r5', 'lr'], ['sp']))
+        self.assertEqual(self.arch.parseArguments(instr('pop', '{r4, r5, lr}')),
+            (['sp'], ['sp', 'r4', 'r5', 'lr']))
+        self.assertEqual(self.arch.parseArguments(instr('pop', '{lr}')),
+            (['sp'], ['sp', 'lr']))
+
+if __name__ == '__main__':
+    unittest.main()
